077fcccdd7c9c1f05d5809a858d7fbd598c9f1e9
tonyfaros <tonyfaros@gmail.com>
2018-02-11 20:40:13 -0600

    agregar imagen a storage en firebase

--- app/src/main/java/com/example/anthony_pc/prueba/MainActivity.java
+++ app/src/main/java/com/example/anthony_pc/prueba/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.anthony_pc.prueba;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.w3c.dom.Text;
 
@@ -18,16 +27,52 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseReference mRootChild = mDatabaseReference.child("nombre");
 
+    private StorageReference mStorage;
+
     private TextView txtView1;
+    private Button btnSubir;
+    private static final int GALLERY_INTENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mStorage = FirebaseStorage.getInstance().getReference();
         txtView1 = (TextView) findViewById(R.id.txtView1);
 
+        btnSubir = (Button) findViewById(R.id.btnSubir);
+
+        btnSubir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY_INTENT);
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+
+            Uri uri = data.getData();
+
+            StorageReference filePath = mStorage.child("fotos").child(uri.getLastPathSegment());
+
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this, "Imagen cargada tuanis", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+
 
     @Override
     protected void onStart() {
