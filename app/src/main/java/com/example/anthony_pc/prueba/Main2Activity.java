c6ff40bf3364b3d02525b192d7e9eb79244651e6
tonyfaros <tonyfaros@gmail.com>
2018-02-15 19:01:25 -0600

    autenticacion

--- app/src/main/java/com/example/anthony_pc/prueba/Main2Activity.java
+++ app/src/main/java/com/example/anthony_pc/prueba/Main2Activity.java
@@ -1,13 +1,116 @@
 package com.example.anthony_pc.prueba;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.w3c.dom.Text;
 
 public class Main2Activity extends AppCompatActivity {
+
+    DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+    DatabaseReference mRootChild = mDatabaseReference.child("nombre");
+
+
+    private StorageReference mStorage;
+    private ImageView mImgView;
+    private ProgressDialog mProgressDialog;
+    private TextView txtView1;
+    private Button btnSubir;
+    private static final int GALLERY_INTENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        txtView1 = (TextView) findViewById(R.id.txtView1);
+        mImgView = (ImageView) findViewById(R.id.imgView);
+        btnSubir = (Button) findViewById(R.id.btnSubir);
+        mProgressDialog = new ProgressDialog(this);
+
+        btnSubir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,GALLERY_INTENT);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+
+            mProgressDialog.setTitle("Subiendo...");
+            mProgressDialog.setMessage("Subiendo foto a firebase..");
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.show();
+
+            Uri uri = data.getData();
+
+            StorageReference filePath = mStorage.child("fotos").child(uri.getLastPathSegment());
+
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgressDialog.dismiss();
+                    Uri descargarFoto = taskSnapshot.getDownloadUrl();
+                    Glide.with(Main2Activity.this)
+                            .load(descargarFoto)
+                            .fitCenter()
+                            //.centerCrop()
+                            .into(mImgView);
+
+                    Toast.makeText(Main2Activity.this, "Imagen cargada tuanis", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        mRootChild.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String texto = dataSnapshot.getValue().toString();
+                txtView1.setText(texto);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
+
--- app/src/main/java/com/example/anthony_pc/prueba/MainActivity.java
+++ app/src/main/java/com/example/anthony_pc/prueba/MainActivity.java
@@ -1,115 +1,128 @@
 package com.example.anthony_pc.prueba;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
-import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseReference mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+    private static final String TAG = "Prueba";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
-    DatabaseReference mRootChild = mDatabaseReference.child("nombre");
+    private Button btnIngresar;
+    private EditText txtCorreo,txtPassword;
 
-    
-    private StorageReference mStorage;
-    private ImageView mImgView;
-    private ProgressDialog mProgressDialog;
-    private TextView txtView1;
-    private Button btnSubir;
-    private static final int GALLERY_INTENT = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mStorage = FirebaseStorage.getInstance().getReference();
-        txtView1 = (TextView) findViewById(R.id.txtView1);
-        mImgView = (ImageView) findViewById(R.id.imgView);
-        btnSubir = (Button) findViewById(R.id.btnSubir);
-        mProgressDialog = new ProgressDialog(this);
+        btnIngresar = (Button) findViewById(R.id.btnIngresar);
+        txtCorreo = (EditText) findViewById(R.id.txtCorreo);
+        txtPassword = (EditText) findViewById(R.id.txtPassw);
 
-        btnSubir.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()!= null){
+                    startActivity(new Intent(MainActivity.this,Main2Activity.class));
+
+                }else{
+                    //Toast.makeText(MainActivity.this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent,GALLERY_INTENT);
-
+                loginUser();
             }
         });
 
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
-
-            mProgressDialog.setTitle("Subiendo...");
-            mProgressDialog.setMessage("Subiendo foto a firebase..");
-            mProgressDialog.setCancelable(false);
-            mProgressDialog.show();
-
-            Uri uri = data.getData();
-
-            StorageReference filePath = mStorage.child("fotos").child(uri.getLastPathSegment());
-
-            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    mProgressDialog.dismiss();
-                    Uri descargarFoto = taskSnapshot.getDownloadUrl();
-                    Glide.with(MainActivity.this)
-                            .load(descargarFoto)
-                            .fitCenter()
-                            //.centerCrop()
-                            .into(mImgView);
-
-                    Toast.makeText(MainActivity.this, "Imagen cargada tuanis", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
-
-
-
-    @Override
-    protected void onStart() {
+    public void onStart() {
         super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
 
-
-        mRootChild.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String texto = dataSnapshot.getValue().toString();
-                txtView1.setText(texto);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
+
+
+    private void loginUserUnkn() {
+        String email = txtCorreo.getText().toString();
+        String password = txtPassword.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    private void loginUser() {
+        String email = txtCorreo.getText().toString();
+        String password = txtPassword.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
+
 }
